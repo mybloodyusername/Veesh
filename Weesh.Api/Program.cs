727af48eba1abab10717b80184b9d12830c0f87d
mybloodyusername <y.sanaei94@gmail.com>
2026-08-15 22:51:07 +0330

    Program.cs basics

--- Weesh.Api/Program.cs
+++ Weesh.Api/Program.cs
@@ -1,17 +1,37 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Logging.ClearProviders().AddConsole();
+builder.Services.AddLogging(options => options.SetMinimumLevel(LogLevel.Trace).AddConsole());
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+builder.Services.AddHttpContextAccessor();
+// TODO: Global Exception Handler
+builder.Services.AddProblemDetails();
+
+// DI 
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+    // TODO: add dev cors
+}
+else
+{
+    // TODO: add prod cors
+}
 
+app.UseExceptionHandler();
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 
 app.MapControllers();
 
